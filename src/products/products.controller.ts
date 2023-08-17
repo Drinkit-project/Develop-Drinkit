2bf34039b5b5823bda16af799eac719730269b98
jangdu <jjd0324@gmail.com>
2023-08-17 23:38:45 +0900

    feat: GET product api 추가

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @ApiOperation({ summary: '상품 등록' })
+  @Get()
+  // Todo: 로그인 추가
+  async getProducts() {
+    const products = await this.productsService.getProducts();
+    return products;
+  }
+}
