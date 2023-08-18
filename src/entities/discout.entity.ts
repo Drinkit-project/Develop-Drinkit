72e2d6ea1bc6dddcbb077ae5c8b15dfcfaa762ac
jangdu <jjd0324@gmail.com>
2023-08-18 23:23:41 +0900

    feat: POST /products/:productId/discount api 추가

--- src/entities/discout.entity.ts
+++ src/entities/discout.entity.ts
@@ -2,19 +2,47 @@ import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 
 import { CommonEntity } from './common.entity';
 import { Product } from './product.entity';
+import { IsDate, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ schema: '', name: 'discount' })
 export class Discount extends CommonEntity {
+  @IsNumber()
+  @ApiProperty({
+    example: '20000',
+    description: '할인 할 가격',
+    required: false,
+  })
   @Column('bigint')
   discountPirce: number;
 
+  @IsNumber()
+  @ApiProperty({
+    example: '30',
+    description: '할인 할 할인율',
+    required: false,
+  })
   @Column('int')
   discountRating: number;
 
+  @IsDate()
+  @ApiProperty({
+    example: '2',
+    description: '할인을 시작하는 날',
+    required: true,
+  })
   @Column('varchar')
   startDate: Date;
 
-  @Column('bigint')
+  @IsDate()
+  @ApiProperty({
+    example: '2',
+    description: '할인이 끝나는 날',
+    required: true,
+  })
+  @Column('varchar')
+  endDate: Date;
+
   productId: number;
 
   @OneToOne(() => Product, (product) => product.discount)
---
+++ src/products/discounts/discounts.service.ts
@@ -0,0 +1,26 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { DiscountsRepository } from './discounts.repository';
+
+@Injectable()
+export class DiscountsService {
+  constructor(private discountsRepository: DiscountsRepository) {}
+
+  async createDiscount(productId, newDiscount) {
+    const { discountPrice, discountRating, startDate, endDate } = newDiscount;
+
+    if (
+      (discountRating && discountPrice) ||
+      (!discountPrice && !discountRating)
+    ) {
+      throw new BadRequestException(
+        '할인 할 가격과 할인율 둘중 한가지를 선택해서 보내주세요.',
+      );
+    }
+
+    const createdDiscount = await this.discountsRepository.createDiscount(
+      productId,
+      newDiscount,
+    );
+    return createdDiscount;
+  }
+}
