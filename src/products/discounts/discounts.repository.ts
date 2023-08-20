37db2776e2def189b3487aa17a2ca1efd8734def
jangdu <jjd0324@gmail.com>
2023-08-20 20:04:43 +0900

    feat: discounts관련 정보 get products에 추가 및 중복할인 시 예외처리

--- src/products/discounts/discounts.repository.ts
+++ src/products/discounts/discounts.repository.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Discount } from 'src/entities/discout.entity';
 import { DataSource, Repository } from 'typeorm';
 
@@ -19,23 +15,16 @@ export class DiscountsRepository extends Repository<Discount> {
     startDate,
     endDate,
   ) {
-    try {
-      const createdDiscount = await this.createQueryBuilder()
-        .insert()
-        .into(Discount)
-        .values({
-          productId,
-          discountPrice,
-          discountRating,
-          startDate,
-          endDate,
-        })
-        .execute();
-    } catch (error) {
-      if (error.code === '23505') {
-        throw new BadRequestException('이미 해당 상품에 할인이 존재합니다.');
-      }
-      throw new InternalServerErrorException('디비에러');
-    }
+    const createdDiscount = await this.createQueryBuilder()
+      .insert()
+      .into(Discount)
+      .values({
+        productId,
+        discountPrice,
+        discountRating,
+        startDate,
+        endDate,
+      })
+      .execute();
   }
 }
