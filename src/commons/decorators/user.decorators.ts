c59baec7faca44ca5f0fba13fc455145f6486eba
yhjs1211 <yhjs1211@gmail.com>
2023-08-22 21:30:08 +0900

    user.decorator update

--- src/commons/decorators/user.decorators.ts
+++ src/commons/decorators/user.decorators.ts
@@ -1,8 +1,31 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  BadRequestException,
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.userId;
+    return request.user;
+  },
+);
+
+export const AdminUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    if (!request.user.isAdmin) throw new BadRequestException('권한 없음');
+
+    return request.user.isAdmin;
+  },
+);
+
+export const PersonalUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    if (!request.user.isPersonal)
+      throw new BadRequestException('권한 없음이지롱~');
+
+    return request.user.isPersonal;
   },
 );
