e73acef96070a855e8db86320a1271a3eba2d4b8
Taehwan Oh <othwan410@daum.net>
2023-08-23 19:39:37 +0900

    Make profile
    프로필 API 1차 완성, 테스트까지는 완료
    추가 보완점 있으면 추후 업데이트

--- src/user/profiles.repository.ts
+++ src/user/profiles.repository.ts
@@ -8,42 +8,88 @@ export class ProfilesRepository extends Repository<Profile> {
     super(Profile, datasource.createEntityManager());
   }
 
+  async addressParse(
+    address: string,
+  ): Promise<Array<{ address: string; name: string }>> {
+    return (address = JSON.parse(address));
+  }
+
   async getProfile(userId: number) {
-    const profile = await this.createQueryBuilder('profile')
+    const profile = await this.createQueryBuilder()
+      .select([
+        'profile.userId',
+        'profile.nickname',
+        'profile.phoneNumber',
+        'profile.name',
+      ])
+      .from(Profile, 'profile')
       .where(`profile.userId = ${userId}`)
-      .getMany();
+      .getOne();
 
     return profile;
   }
 
   async getAddress(userId: number) {
-    const address = await this.createQueryBuilder('profile')
-      .select('address')
-      .where('profile.userId = :userId', { userId })
-      .getMany();
+    const addressString = await this.createQueryBuilder()
+      .select('profile.address')
+      .from(Profile, 'profile')
+      .where(`profile.userId = ${userId}`)
+      .getOne();
+
+    console.log(addressString);
+
+    const address = await this.addressParse(addressString.address);
 
     return address;
   }
 
-  async getPaymentLog(paymentLogId: number) {
-    const getPaymentLogData = await this.createQueryBuilder('paymentLog')
-      .where('paymentLog.id = :paymentLogId', { paymentLogId })
-      .getOne();
+  async addAddress(
+    userId: number,
+    newAddress: { address: string; name: string },
+  ) {
+    const address = await this.getAddress(userId);
+    address.push(newAddress);
 
-    return getPaymentLogData;
+    return await this.createQueryBuilder()
+      .update(Profile)
+      .set({ address: JSON.stringify(address) })
+      .where(`userId = :userId`, { userId })
+      .execute();
   }
 
-  async getStoreOrders(storeId: number) {
-    const getStoreOrdersData = await this.createQueryBuilder('paymentLog')
-      .where('paymentLog.storeId = :storeId', { storeId })
-      .getMany();
-    return getStoreOrdersData;
+  async updateOrderAddress(userId: number, addressIdx: number) {
+    const address = await this.getAddress(userId);
+    const [tempAddress] = address.splice(addressIdx, 1);
+    address.unshift(tempAddress);
+
+    return await this.createQueryBuilder()
+      .update(Profile)
+      .set({ address: JSON.stringify(address) })
+      .where(`userId = :userId`, { userId })
+      .execute();
   }
 
-  async getAdminOrders() {
-    const getAdminOrdersData = await this.createQueryBuilder('paymentLog')
-      .where('paymentLog.storeId = :storeId', { storeId: 1 })
-      .getMany();
-    return getAdminOrdersData;
+  async updateAddress(
+    userId: number,
+    newAddress: { address: string; name: string },
+    addressIdx: number,
+  ) {
+    const address = await this.getAddress(userId);
+    address[addressIdx] = newAddress;
+    return await this.createQueryBuilder()
+      .update(Profile)
+      .set({ address: JSON.stringify(address) })
+      .where(`userId = :userId`, { userId })
+      .execute();
+  }
+
+  async deleteAddress(userId: number, addressIdx: number) {
+    const address = await this.getAddress(userId);
+    address.splice(addressIdx, 1);
+    return await this.createQueryBuilder()
+      .update(Profile)
+      .set({ address: JSON.stringify(address) })
+      .where(`userId = :userId`, { userId })
+      .execute();
   }
 }
