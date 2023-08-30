f54af5441d3d6806822442f28636b063c2ea841f
Nam HyunJin <skaguswls200@naver.com>
2023-08-31 02:41:53 +0900

    feat: openSearch 프론트 연동
    openSearch 프론트 연동 / stores response ImgURL 추가

--- src/open-search/dto/open-search.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class OpenSearchDto {
-  @IsString()
-  readonly keyword: string;
-}
--- src/open-search/open-search.controller.ts
+++ src/open-search/open-search.controller.ts
@@ -1,14 +1,13 @@
-import { Controller, Get, Body } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { OpenSearchService } from './open-search.service';
-import { OpenSearchDto } from './dto/open-search.dto';
 
 @Controller('open-search')
 export class OpenSearchController {
   constructor(private readonly openSearchService: OpenSearchService) {}
 
   @Get()
-  async getSearch(@Body() body: OpenSearchDto) {
-    const getSearchData = await this.openSearchService.getSearch(body.keyword);
+  async getSearch(@Query('keyword') keyword: string) {
+    const getSearchData = await this.openSearchService.getSearch(keyword);
     return getSearchData;
   }
 }
